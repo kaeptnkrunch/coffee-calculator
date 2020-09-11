8fb5c5c704852fc1dcc0d2c50c0b853acc1b33c1
kaeptnkrunch <55900780+kaeptnkrunch@users.noreply.github.com>
2020-09-11 09:58:33 +0200

    Update coffeecalculator.ps1

--- coffeecalculator.ps1
+++ coffeecalculator.ps1
@@ -15,23 +15,63 @@ Write-Host "Heute haben wir den $datum `n"
 # Hinweis
 Write-Host "Hinweis: Dezimalstellen werden durch einen Punkt angegeben z.B. 0.10€"
 
-# Eingaben 
+# Abfrage Kaffee in Tassen
 $Input1 = Read-Host "Gebe das Gewicht der Kaffeepackung in Gramm an"
 $Input2 = Read-Host "Geb bitte den Preis für die Kaffeepackung in € an"
 $Input3 = Read-Host "Geb bitte die Marge für den Preis pro Kaffee in € an"
-$Input4 = Read-Host "Gib die Menge an verwendete Kaffeepulver in Gramm an"
+$Input0 = Read-Host "Möchten Sie eine ganze Kanne Kaffee machen?"
 
-# Zeilenumbruch
-Write-Host "`n"
 
-# Beispielrechnung
+# Beispielrechnung (1 Kanne Kaffee)
 # x g Kaffee / 70g pro Portion = 2 L Kaffee / 10 = Mege Kaffees aus 2L Kaffee = Preis pro 200ml Tasse + 0,05€; 0,10€ Marge
 # Beispiel: 500g(11,50€)/70g=7,14 Portionen (1,61€ 2L)/10 = 0,16€ pro 200ml + 0,05€; 0,10€ Marge (Bsp: 0,16€ + 0,10€ Marge = 0,26€ pro 200ml Tasse Kaffee)
 
-# Berechnung
-$calculation = ($Input1 / $Input4) 
-$calculation2 = ($Input2 / $calculation) / 10 + $Input3
+# like Anfrage
+if($Input0 -like "Ja")
+{
+    # Berechnung
+    $calculation = ($Input1 / 70) 
+    $calculation2 = ($Input2 / $calculation) / 10 + $Input3
 
-# Ausgabe
-Write-Host "Mit dem Gewicht von $Input1 g erhälst du $calculation Kannen Kaffee"
-Write-Host "Der Preis pro 200ml Tasse Kaffee ist $calculation2 € mit einer Marge von $Input3 €"
+    # Zeilenumbruch
+    Write-Host "`n"
+
+    # Ausgabe
+    Write-Host "Mit dem Gewicht von $Input1 g erhälst du $calculation Kannen Kaffee"
+    Write-Host "Der Preis pro 200ml Tasse Kaffee ist $calculation2 € mit einer Marge von $Input3 €"
+
+} else {
+
+    # Beispielrechnung (1 Tassee Kaffee)
+    # 70g = 2L Kaffee/200ml = 10 Tassen Kaffee = 70/10=7g Kaffee pro Tasse
+
+    # Input Tassen Kaffee
+    $objForm.Controls.Add($objTextBox)
+    $Input4 = Read-Host "Wieviele Tassen Kaffee möchten Sie machen?"
+    
+    # Zeilenumbruch
+    Write-Host "`n"
+
+    # Berechnung pro Tasse
+    $calculation3 = (7 * $Input4)
+
+    # Berechnung
+    $calculation = ($Input1 / 70) 
+    $calculation2 = ($Input2 / $calculation) / 10 + $Input3
+
+    # Ausgabe Kaffee pro Tasse
+    Write-Host "Für $Input4 Tassen Kaffee brauchen Sie $calculation3 g Kaffee"
+
+    if ($Input4 -eq 10)
+    {
+        # Zeilenumbruch
+        echo "Dann kannst du auch gleich eine ganze Kanne Kaffee mit 70g Kaffee machen!"
+        
+    } else { 
+            Write-Host "`n"
+            }
+    
+    # Ausgabe 2
+    Write-Host "Mit dem Gewicht von $Input1 g erhälst du $calculation Kannen Kaffee"
+    Write-Host "Der Preis pro 200ml Tasse Kaffee ist $calculation2 € mit einer Marge von $Input3 €"
+}
